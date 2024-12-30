2f933f3209eccf6d217b7fd079c327271af07a4b
Robbe <robbevaes95@gmail.com>
2024-12-30 11:59:05 +0100

    feat: updated translations

--- apps/nuxt/app/components/header/TheHeader.vue
+++ apps/nuxt/app/components/header/TheHeader.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { VcIcon } from '@wisemen/vue-core'
-
 import HeaderLogo from '@/components/header/HeaderLogo.vue'
 import TheLocaleSelector from '@/components/locale/TheLocaleSelector.vue'
 import { NuxtLinkLocale } from '#components'
@@ -10,20 +8,7 @@ const { t } = useI18n()
 
 <template>
   <header class="fixed top-0 z-20 w-full">
-    <div class="bg-brand-primary text-brand-primary">
-      <div class="container mx-auto px-4 py-2">
-        <div class="flex items-center gap-2">
-          <span class="text-xs text-gray-400">
-            {{ t('app.header.compatible_with_vektron') }}
-          </span>
-          <VcIcon
-            icon="chevronRight"
-            class="size-lg"
-          />
-        </div>
-      </div>
-    </div>
-    <div class="bg-gradient-to-b from-[rgb(0,0,0,0.8)] to-transparent py-6 text-white">
+    <div class="bg-black to-transparent py-6 text-white">
       <div class="container mx-auto px-4">
         <div class="flex items-center justify-between text-white lg:grid lg:grid-cols-3 lg:items-center lg:justify-center">
           <HeaderLogo />
