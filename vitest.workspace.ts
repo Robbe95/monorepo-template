5edd8ae028e8662440925f771908b24c7ea270da
Robbe <robbevaes95@gmail.com>
2024-12-30 13:45:27 +0100

    feat: setup vitest

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,4 +1,22 @@
-export default [
-  'packages/*',
-  'apps/*',
-]
+import { defineWorkspace } from 'vitest/config'
+
+export default defineWorkspace([
+  {
+    test: {
+      include: ['**/apps/nuxt/**/*.test.{ts,js}'],
+      name: 'nuxt',
+    }
+  },
+  {
+    test: {
+      include: ['**/apps/payload/**/*.test.{ts,js}'],
+      name: 'payload',
+    }
+  },
+  {
+    test: {
+      include: ['**/packages/**/*.test.{ts,js}'],
+      name: 'packages',
+    }
+  }
+])
